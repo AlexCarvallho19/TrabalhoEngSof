f279daa6fbdf7aaa9e48b8cecac088b3a7cdd8a7
mayconSouza21 <61014262+mayconSouza21@users.noreply.github.com>
2020-02-13 11:12:30 -0300

    Update LekaoGod.java

--- EngSoft/src/LekaoGod.java
+++ EngSoft/src/LekaoGod.java
@@ -10,7 +10,7 @@ public class LekaoGod {
 
 		num2 = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite outro numero:"));
 
-		soma = num1 + num2; JOptionPane.showMessageDialog(null,"A soma dos numeros e: " + somasaaa);
+		soma = num1 + num2; JOptionPane.showMessageDialog(null,"A soma dos numeros e: " + soma);
 		 }
 } 
 
